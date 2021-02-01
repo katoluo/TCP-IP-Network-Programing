6bd2634d25f3c5647f5cc76f73431579bc5cbc7f
kato <1123629751@qq.com>
2021-02-01 22:24:06 +0800

    新文件：   chapter_09/README.md 	新文件：   chapter_09/get_buf.c 	新文件：   chapter_09/getbuf 	新文件：   "chapter_09/images/\345\233\2769-1.png" 	新文件：   "chapter_09/images/\345\233\2769-2.png" 	新文件：   "chapter_09/images/\345\233\2769-3.png" 	新文件：   "chapter_09/images/\350\241\2509-1.png" 	新文件：   "chapter_09/images/\350\277\220\350\241\214\347\273\223\346\236\234.png" 	新文件：   "chapter_09/images/\350\277\220\350\241\214\347\273\223\346\236\2342.png" 	新文件：   "chapter_09/images/\350\277\220\350\241\214\347\273\223\346\236\2343.png" 	新文件：   chapter_09/reuseaddr_eserver.c 	新文件：   chapter_09/set_buf.c 	新文件：   chapter_09/setbuf 	新文件：   chapter_09/sock_type.c 	新文件：   chapter_09/socktype

---
+++ chapter_09/reuseaddr_eserver.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define TRUE 1
+#define FALSE 0
+void error_handing(char *message);
+
+int main(int argc, char *argv[])
+{
+  int serv_sock, clnt_sock;
+  char message[30];
+  int option, str_len;
+  socklen_t optlen, clnt_adr_sz;
+  struct sockaddr_in serv_addr, clnt_addr;
+  if (argc != 2) {
+    printf("Usage: %s <port> \n", argv[0]);
+    exit(1);
+  }
+
+  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (serv_sock == -1)
+    error_handing("socket() error");
+
+  /*
+  optlen = sizeof(option);
+  option = TRUE;
+  setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, (void*)&option,
+      optlen);
+  */
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(atoi(argv[1]));
+
+  if (bind(serv_sock, (struct sockaddr*)&serv_addr, 
+        sizeof(serv_addr)) == -1)
+    error_handing("bind() error");
+  if (listen(serv_sock, 5) == -1)
+    error_handing("listen() error");
+  clnt_adr_sz = sizeof(clnt_addr);
+  clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, 
+      &clnt_adr_sz);
+
+  while ((str_len = read(clnt_sock, message, sizeof(message))) != 0)
+  {
+    write(clnt_sock, message, str_len);
+    write(1, message, str_len);
+  }
+  close(clnt_sock);
+  close(serv_sock);
+  return 0;
+}
+
+void error_handing(char *message)
+{
+  fputs(message, stderr);
+  fputc('\n', stderr);
+  exit(1);
+}
